1dbebc72acf01dba5858ef6613b6cd7dcad08744
Dmitry-nm22 <naumenkodima209@gmail.com>
2019-10-06 19:03:59 +0300

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Header = () => {
+    return  <header className='header'>
+        <img src ='https://i.pinimg.com/736x/33/b8/69/33b869f90619e81763dbf1fccc896d8d--lion-logo-modern-logo.jpg ' />
+    </header>
+}
+
+export default Header;
