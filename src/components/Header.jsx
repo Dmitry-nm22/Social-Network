49a1112a9f186ff6d5893a643713c62e412ef4c4
Dmitry-nm22 <naumenkodima209@gmail.com>
2019-10-06 20:18:29 +0300

    commit 15

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import styles from './Header.module.css';
 
 const Header = () => {
-    return  <header className='header'>
+    return  <header className={styles.header}>
         <img src ='https://i.pinimg.com/736x/33/b8/69/33b869f90619e81763dbf1fccc896d8d--lion-logo-modern-logo.jpg ' />
     </header>
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import styles from './Profile.module.css';
 
 const Profile = () => {
-    return  <div className='content'>
+    return  <div className={styles.content}>
         <div>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlEVRgcMNTFiSVjmb4_NxD9sIjgCWpkos01luUIk9XVBal-d8l&s" />
         </div>
@@ -14,11 +14,11 @@ const Profile = () => {
             <div>
                 New post
             </div>
-            <div>
-                <div>
+            <div className={styles.Posts}>
+                <div className={styles.item}>
                     Post 1
                 </div>
-                <div>
+                <div className={styles.item}>
                     Post 2
                 </div>
             </div>
