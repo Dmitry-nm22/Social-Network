bc18460b7be7507144bef77cbb8d59f55e9c691a
Dmitry-nm22 <naumenkodima209@gmail.com>
2019-10-31 20:51:36 +0300

    add Messages component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,27 @@ import style from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                <div className={style.dialog + ' ' + style.active}>
+                    Angelina
+                </div>
+                <div className={style.dialog}>
+                    Sasha
+                </div>
+                <div className={style.dialog}>
+                    Denis
+                </div>
+                <div className={style.dialog}>
+                    Ilya
+                </div>
+
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>hi</div>
+                <div className={style.message}>yo</div>
+                <div className={style.message}>qu</div>
+            </div>
         </div>
     )
 }
