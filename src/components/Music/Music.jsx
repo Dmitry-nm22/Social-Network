f1f13ae59a3e9793b99b0d47d219843d5316bcdc
Dmitry-nm22 <naumenkodima209@gmail.com>
2019-10-31 19:48:23 +0300

    add router-dom

---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Music.module.css';
+
+const News = () => {
+    return (
+        <div>
+            Music
+        </div>
+    )
+}
+
+
+export default News;
---
+++ src/components/Settings/Settings.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Settings.module.css';
+
+const News = () => {
+    return (
+        <div>
+            Settings
+        </div>
+    )
+}
+
+
+export default News;
