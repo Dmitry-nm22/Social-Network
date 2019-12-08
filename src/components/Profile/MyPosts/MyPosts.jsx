30104a139c3cacf527cb7722ed8fddcbf88fcfa9
Dmitry-nm22 <naumenkodima209@gmail.com>
2019-12-08 18:15:49 +0300

    refactoring MyPost component

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -10,15 +10,20 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef();
 
      let addPost = () =>{
+        props.addPost();
+    }
+
+    let onPostChange = () =>{
         let text = newPostElement.current.value;
-        props.addPost(text)
+        props.updateNewPostText(text);
+
     }
 
     return <div className={styles.postsBlock}>
         <h3>My posts</h3>
         <div>
             <div>
-                <textarea ref={newPostElement}></textarea>
+                <textarea ref={newPostElement} value={props.newPostText} onChange={onPostChange}/>
             </div>
             <div>
                 <button onClick={addPost}>add Post</button>
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+import {rerenderEntireTree} from "../render";
+
 let state = {
     profilePage: {
         posts: [
@@ -5,7 +7,8 @@ let state = {
             {id: 2, message: 'Its my first post', likesCount: 11},
             {id: 2, message: 'blablabla', likesCount: 13},
             {id: 2, message: 'hey yo', likesCount: 11}
-        ]
+        ],
+        newPostText: 'blablabla'
 
     },
     dialogsPage: {
@@ -27,13 +30,21 @@ let state = {
 }
 
 
-export  let addPost = (postMessage) =>{
+export  let addPost = () =>{
     let newPost = {
         id: 5,
-        message: postMessage,
+        message: state.profilePage.newPostText,
         likesCount: 0
     };
     state.profilePage.posts.push(newPost);
+    state.profilePage.newPostText = ''
+    rerenderEntireTree(state);
+}
+
+export  let updateNewPostText = (newText) =>{
+
+    state.profilePage.newPostText = newText;
+    rerenderEntireTree(state);
 }
 
 export default state;
