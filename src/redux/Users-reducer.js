3a2e843e74147013e9129c1d9cd32ad3b806ce5e
Dmitry-nm22 <naumenkodima209@gmail.com>
2020-02-15 00:17:49 +0300

    refactoring code

--- src/redux/Users-reducer.js
+++ src/redux/Users-reducer.js
@@ -52,11 +52,11 @@ let initialState = {
       }
 };
 
-export const followAC = (userId) =>( {type: FOLLOW, userId} );
-export const unfollowAC = (userId) => ( {type: UNFOLLOW , userId} );
-export const usersAC = (users) => ( {type: SET_USERS , users} );
-export const setCurrentPageAC = (currentPage) => ( {type: SET_CURRENT_PAGE , currentPage} );
-export const setTotalUsersCountAC = (count) => ( {type: SET_TOTAL_USERS_COUNT , count} );
-export const toggleIsFetchingAC = (isFetching) => ( {type: TOGGLE_IS_FETCHING , isFetching } );
+export const follow = (userId) =>( {type: FOLLOW, userId} );
+export const unfollow = (userId) => ( {type: UNFOLLOW , userId} );
+export const setUsers = (users) => ( {type: SET_USERS , users} );
+export const setCurrentPage = (currentPage) => ( {type: SET_CURRENT_PAGE , currentPage} );
+export const setTotalUsersCount = (count) => ( {type: SET_TOTAL_USERS_COUNT , count} );
+export const toggleIsFetching = (isFetching) => ( {type: TOGGLE_IS_FETCHING , isFetching } );
 
 export default UsersReducer;
