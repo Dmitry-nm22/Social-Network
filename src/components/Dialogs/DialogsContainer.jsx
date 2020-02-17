487d960198055fe6a3f4bad44c0521461695b139
Dmitry-nm22 <naumenkodima209@gmail.com>
2020-02-17 23:27:42 +0300

    add HOC

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/Dialogs-reducer";
 import Dialogs from "./Dialogs";
 import {connect} from "react-redux";
+import {Redirect} from "react-router-dom";
+import {withAuthRedirect} from "../../hoc/WithAuthRedirect";
+
+
 
 
 let mapStateToProps = (state) =>{
@@ -21,7 +25,10 @@ let mapDispatchTooProps = (dispatch) =>{
     }
 };
 
-const DialogsContainer = connect(mapStateToProps, mapDispatchTooProps) (Dialogs);
+let AuthRedirectComponent = withAuthRedirect(Dialogs);
+
+
+const DialogsContainer = connect(mapStateToProps, mapDispatchTooProps) (AuthRedirectComponent);
 
 
 export default DialogsContainer;
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,6 +3,7 @@ import Profile from "./Profile";
 import {connect} from "react-redux";
 import {getUserProfile} from "../../redux/Profile-reducer";
 import {Redirect, withRouter} from "react-router-dom";
+import {withAuthRedirect} from "../../hoc/WithAuthRedirect";
 
 
 
@@ -18,19 +19,19 @@ class ProfileContainer extends React.Component{
 
 
     render(){
-        if(!this.props.isAuth) return <Redirect to={'/login'}/>
         return <div>
             <Profile {...this.props} profile={this.props.profile}/>
         </div>
     }
 }
 
+let AuthRedirectComponent = withAuthRedirect(ProfileContainer)
 
 let mapStateToProps = (state) =>({
     profile: state.profilePage.profile,
     isAuth: state.auth.isAuth
 })
 
-let WithUrlDataContainerComponent =  withRouter(ProfileContainer)
+let WithUrlDataContainerComponent =  withRouter(AuthRedirectComponent)
 
 export default connect (mapStateToProps, {getUserProfile} )( WithUrlDataContainerComponent);
