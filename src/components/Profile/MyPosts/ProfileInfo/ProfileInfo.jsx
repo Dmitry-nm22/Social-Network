30ac30626eab74c5b78e363aed141dea1196a149
Dmitry-nm22 <naumenkodima209@gmail.com>
2020-02-22 16:09:26 +0300

    add status bar

--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './ProfileInfo.module.css';
 import Preloader from "../../../Common/Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus";
 
 
 
@@ -10,14 +11,9 @@ const ProfileInfo = (props) => {
         return <Preloader/>
     }
     return <div>
-        <div>
-            <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlEVRgcMNTFiSVjmb4_NxD9sIjgCWpkos01luUIk9XVBal-d8l&s"/>
-        </div>
         <div className={styles.description_block}>
-
             <img src={props.profile.photos.large} alt=""/>
-            ava + description
+            <ProfileStatus status={'hello'}/>
         </div>
     </div>
 
