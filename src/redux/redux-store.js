046ba21d15a2c8cb56d28a06f47220467ca74681
Dmitry-nm22 <naumenkodima209@gmail.com>
2020-02-26 23:28:08 +0300

    add redux-form

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,13 +5,15 @@ import sidebarReducer from "./Sidebar-reducer";
 import UsersReducer from "./Users-reducer";
 import authReducer from "./auth-reducer";
 import  thunkMiddleWare from "redux-thunk";
+import {reducer as formReducer} from "redux-form"
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
     usersPage: UsersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleWare));
